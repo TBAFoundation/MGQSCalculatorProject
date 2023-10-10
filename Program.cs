5420022582fbc4d96262cda3ee32bdd9ea476439
Tahir Bastu Abiodun <abdullahabeyking@gmail.com>
2023-10-10 15:09:38 +0100

    Calculator App for Basic Operation

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿// SIMPLE CALCULATOR  APP
+Console.Title = "=====Simple Calculator=====";
+Console.WriteLine("=====Simple Calculator=====");
+double result;
+bool flag = true;
+try
+{
+    double firstOperand = 0;
+    double secondOperand =  0;
+    while(flag)
+    {
+        Console.WriteLine("Enter: \n1. + or add for Addition\n2. - or subtract for Subtraction\n3. * or multiply for Multiplication\n4. / or divide for Division");
+        string operatorInput = Console.ReadLine()!;
+        switch(operatorInput.ToLower()!)
+        {
+            case "+":
+            case "add":
+            Console.WriteLine("Enter first operand: ");
+            firstOperand = double.Parse(Console.ReadLine()!);
+            Console.WriteLine("Enter second operand: ");
+            secondOperand = double.Parse(Console.ReadLine()!);
+            result = firstOperand + secondOperand;
+            Console.WriteLine($"{firstOperand} + {secondOperand} = {Math.Round(result, 2)}");
+            break;
+
+            case "-":
+            case "subtract":
+            Console.WriteLine("Enter first operand: ");
+            firstOperand = double.Parse(Console.ReadLine()!);
+            Console.WriteLine("Enter second operand: ");
+            secondOperand = double.Parse(Console.ReadLine()!);
+            result = firstOperand  - secondOperand;
+            Console.WriteLine($"{firstOperand} - {secondOperand} = {Math.Round(result, 2)}");
+            break;
+
+            case "*":
+            case "multiply":
+            Console.WriteLine("Enter first operand: ");
+            firstOperand = double.Parse(Console.ReadLine()!);
+            Console.WriteLine("Enter second operand: ");
+            secondOperand = double.Parse(Console.ReadLine()!);
+            result = firstOperand * secondOperand;
+            Console.WriteLine($"{firstOperand} * {secondOperand} = {Math.Round(result, 2)}");
+            break;
+
+            case "/":
+            case "divide":
+            Console.WriteLine("Enter first operand: ");
+            firstOperand = double.Parse(Console.ReadLine()!);
+            Console.WriteLine("Enter second operand: ");
+            secondOperand = double.Parse(Console.ReadLine()!);
+            result = firstOperand / secondOperand;
+            if (secondOperand == 0)
+            {
+                throw new Exception();
+            }
+            Console.WriteLine($"{firstOperand} / {secondOperand} = {Math.Round(result, 2)}");
+            break;
+        }
+        string option;
+        do
+        {
+            Console.WriteLine("Do you want perform another calculation?(y for yes / n for no)");
+            option = Console.ReadLine()!.ToLower();
+
+        }while(!option.Equals("y".ToLower()) && !option.Equals("n".ToLower()));
+        flag = option.Equals("y".ToLower());
+
+        if(!flag)
+        {
+            Console.WriteLine("Thank you for using our calculator application");
+        }
+    }
+}    
+catch(Exception)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("An Error Ocurred!");
+    Console.ResetColor();
+}
